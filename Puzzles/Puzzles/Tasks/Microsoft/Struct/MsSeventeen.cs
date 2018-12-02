d3553c85dcc4dd9ad2b6314c88e52bcdad6b573a
Deepak <Deepakgullipalli@gmail.com>
2018-12-02 22:54:53 +0530

    ms 17

---
+++ Puzzles/Puzzles/Tasks/Microsoft/Struct/MsSeventeen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzles.Tasks.Microsoft.Struct
+{
+    class MsSeventeen
+    {
+        abc e = new abc();
+    }
+
+    struct abc
+    {
+        public string name;
+        internal int age;
+        private Single sal;
+    }
+}
